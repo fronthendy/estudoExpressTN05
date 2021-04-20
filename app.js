aa31ffc467a7019094ef5de63421e5aa97b87db6
Hendy Almeida <hendy.almeida@hotmail.com>
2021-04-19 22:03:18 -0300

    create usuarios

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+//indica para express que usaremos os dados em JSON
+app.use(express.json());
+
 let usuarios = [
     {nome: "Hendy", sobrenome: "Almeida"}
 ];
@@ -12,10 +15,28 @@ app.get("/inicio", (request, response) => {
 
 // http://localhost:3000/usuarios
 app.get("/usuarios", (request, response) => {
-    // console.log(request);
     return response.json(usuarios);
 })
 
-app.listen(3000, () => {
+// http://localhost:3000/usuarios
+app.post("/usuarios", (request, response) => {
+    const { nome, sobrenome } = request.body;
+
+    usuarios.push({ nome, sobrenome });
+
+    return response.json({ nome, sobrenome });
+})
+
+// http://localhost:3000/usuarios/hendy
+app.put("/usuarios/:nome", (request, response) => {
+    const { nome } = request.params;
+
+    const { sobrenome } = request.body;
+
+
+
+})
+
+app.listen(3000, (req, res) => {
     console.log("Servidor rodando")
 })
